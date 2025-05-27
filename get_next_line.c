ca6da0718319ea5f6482f7cb74b46abd478f217c
Milena Egiazarian <megiazar@c2r3s3.42lisboa.com>
2025-05-27 19:14:39 +0100

    final

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: megiazar <megiazar@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 18:07:34 by megiazar          #+#    #+#             */
-/*   Updated: 2025/05/27 17:04:32 by megiazar         ###   ########.fr       */
+/*   Updated: 2025/05/27 18:16:55 by megiazar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 char	*get_next_line(int fd)
 {
-	int				read_bytes;
+	int				count_bytes;
 	static char		*store;
 	char			*line;
 	char			*tmp;
@@ -24,16 +24,16 @@ char	*get_next_line(int fd)
 	tmp = malloc(BUFFER_SIZE + 1);
 	if (!tmp)
 		return (free(tmp), NULL);
-	read_bytes = 1;
-	while (!has_newline(store) && read_bytes > 0)
+	count_bytes = 1;
+	while (!has_newline(store) && count_bytes > 0)
 	{
-		read_bytes = read(fd, tmp, BUFFER_SIZE);
-		if (read_bytes == -1)
+		count_bytes = read(fd, tmp, BUFFER_SIZE);
+		if (count_bytes == -1)
 			return (free(tmp), free(store), store = NULL, NULL);
-		tmp[read_bytes] = '\0';
+		tmp[count_bytes] = '\0';
 		store = str_join_and_free(store, tmp);
 		if (!store)
-			return (free(tmp), NULL);
+			return (free(tmp), free(store), store = NULL, NULL);
 	}
 	free(tmp);
 	line = next_line(store);
@@ -47,24 +47,19 @@ int main(int ac, char **av)
 {
     (void)ac;
     char *line;
-    int fd1 = open("example.txt", O_RDONLY);
+    int fd1 = open(av[1], O_RDONLY);
 
     if (fd1 < 0)
     {
         perror("EEEW errors opening file\n");
         return (-1);
     }
-/*     if (binary_or_bipolar(fd1))
-    {
-        printf("EEEEEWWWWW its a binary file\n");
-        close(fd1);
-        return (0);
-    }  */
-    while ((line = get_next_line(fd1)) != NULL)
+	while ((line = get_next_line(fd1)) != NULL)
     {
         printf("%s", line);
         free(line);
     }
+	printf("%s", get_next_line(fd1));
     close(fd1);
     return (0);
 } 
